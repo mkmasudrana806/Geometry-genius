777239d37b54e627242c6d08ecbd6693c7df6d9f
jabed366 <jabedhasan231@gmail.com>
2023-02-18 21:13:35 +0600

    validation for negative,string and empty value

--- js/function.js
+++ js/function.js
@@ -12,35 +12,36 @@ function area_calculation(geo_name, geo_base, geo_height) {
     const height = document.getElementById(geo_height);
     const geometry_name = document.getElementById(geo_name).innerText;
 
-    //if values are not entered
-    if (base.value == '' || height.value == '') {
-        alert('Please enter the values');
-        return;
-    } else {
+    //check value is string or negative value or empty value
 
-        if (geometry_name =="Rectangular" || geometry_name=="Parallelogram") {
-            gio_area =parseFloat( (base.value * height.value).toFixed(2))
-        }
-        else if(geometry_name =="Ellipse"){
-            gio_area =parseFloat( ( (22/7) *base.value * height.value).toFixed(2))
+    if (base.value == '' || height.value == '' || isNaN(base.value) || isNaN(height.value) || base.value < 0 || height.value < 0) {
+        
+            alert('Please enter the values');
+            return;
         } 
         else {
-            gio_area =parseFloat( (.5 * base.value * height.value).toFixed(2));
+
+            if (geometry_name == "Rectangular" || geometry_name == "Parallelogram") {
+                gio_area = parseFloat((base.value * height.value).toFixed(2))
+            } else if (geometry_name == "Ellipse") {
+                gio_area = parseFloat(((22 / 7) * base.value * height.value).toFixed(2))
+            } else {
+                gio_area = parseFloat((.5 * base.value * height.value).toFixed(2));
+
+            }
+            //pushing the data to the array
+            area_data.push({
+                base: base.value,
+                height: height.value,
+                area: gio_area,
+                name: geometry_name
+
+            });
+            all_geometry_area();
+            document.getElementById(geo_base).value = '';
+            document.getElementById(geo_height).value = '';
+
 
         }
-        //pushing the data to the array
-        area_data.push({
-            base: base.value,
-            height: height.value,
-            area: gio_area,
-            name: geometry_name
-
-        });
-        all_geometry_area();
-        document.getElementById(geo_base).value='';
-        document.getElementById(geo_height).value='';
-
 
     }
-
-}
