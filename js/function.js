54c7fba175a65b7e135b7f88bcb584280f11e749
jabed366 <jabedhasan231@gmail.com>
2023-02-18 21:08:17 +0600

    function file created and move function here

---
+++ js/function.js
@@ -0,0 +1,46 @@
+/**
+ * 
+ * @param {*} geo_name is geometry shape name 
+ * @param {*} geo_base  height
+ * @param {*} geo_height width
+ * @returns 
+ */
+function area_calculation(geo_name, geo_base, geo_height) {
+
+    //selector for the form
+    const base = document.getElementById(geo_base);
+    const height = document.getElementById(geo_height);
+    const geometry_name = document.getElementById(geo_name).innerText;
+
+    //if values are not entered
+    if (base.value == '' || height.value == '') {
+        alert('Please enter the values');
+        return;
+    } else {
+
+        if (geometry_name =="Rectangular" || geometry_name=="Parallelogram") {
+            gio_area =parseFloat( (base.value * height.value).toFixed(2))
+        }
+        else if(geometry_name =="Ellipse"){
+            gio_area =parseFloat( ( (22/7) *base.value * height.value).toFixed(2))
+        } 
+        else {
+            gio_area =parseFloat( (.5 * base.value * height.value).toFixed(2));
+
+        }
+        //pushing the data to the array
+        area_data.push({
+            base: base.value,
+            height: height.value,
+            area: gio_area,
+            name: geometry_name
+
+        });
+        all_geometry_area();
+        document.getElementById(geo_base).value='';
+        document.getElementById(geo_height).value='';
+
+
+    }
+
+}
